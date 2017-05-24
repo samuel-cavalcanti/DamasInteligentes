5584e02b2ff64e418fcdc7237cad863dca2d7e17
samuel-cavalcanti <scavalcanti111@gmail.com>
2017-05-24 13:01:07 -0300

    add verificador de casa

--- myLibraries/Casas.js
+++ myLibraries/Casas.js
@@ -5,18 +5,22 @@ function Casa() {
 
   this.pos = [0, 0];
 
+  this.busy = false;
+
   this.addDama = function(peça) {
     peça.pos.set(this.pos[0], this.pos[1]);
+    this.busy =true;
   };
 
-  this.rmDama = function() {
-    this.pos = [0, 0];
+  this.rmDama = function(peça) {
+    peça.pos = [0, 0];
   };
 
   this.mvDama = function(posInicial, posFinal) {
     posFinal.addDama(peça);
     posInicial.rmDama();
 
-
   };
+
+
 }
